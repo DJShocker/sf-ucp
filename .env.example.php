cc83e90a4e700ce19bc49fbd64b24a26d9530a05
Lorenc Pekaj <zeelorenc@hotmail.com>
2022-01-09 19:11:59 +1100

    v1 dockerization

---
+++ .env.example.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Make sure you copy this file into .env.php else the application will not work!
+ */
+
+return [
+    // Debug Mode
+    'DEBUG' => 1,
+
+    // Database
+    'DB_PORT' => '3306',
+    'DB_HOST' => '127.0.0.1',
+    'DB_DATABASE' => 'database',
+    'DB_USER' => 'username',
+    'DB_PASSWORD' => 'password',
+];
